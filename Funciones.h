9526af3e485ece0cdb4bd9887b95030ac2e6f32f
GarAngelGF <agarciaf2102@alumno.ipn.mx>
2025-10-14 22:50:12 -0600

    Sopadepapa

---
+++ Funciones.h
@@ -0,0 +1,15 @@
+#ifndef METODOSDEORDENAMIENTOBUSQUEDA_FUNCIONES_H
+#define METODOSDEORDENAMIENTOBUSQUEDA_FUNCIONES_H
+
+
+void llenarArreglo(int arreglo[],int tamano);
+void mostrarArreglo(const int arreglo[], int tamano);
+void busquedaSecuencia(const int arreglo[], int tamano,int valor);
+void busquedaBinaria(const int arreglo[], int tamano);
+void ordenamientoBurbuja(const int arreglo[], int tamano);
+void ordenamientoSeleccion(const int arreglo[], int tamano);
+void ordenamientoInsersion(const int arreglo[], int tamano);
+
+
+
+#endif
